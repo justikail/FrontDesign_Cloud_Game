b93a197b02866b3a4f0e9f6a589b8f2d80885ac2
justikail <mininginformation1337@gmail.com>
2024-02-13 09:51:21 +0700

    first commit

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,36 @@ function fs() {
     if (e.key == "F11") e.preventDefault();
   });
 }
+
+const sliderContainers = document.querySelectorAll(".slider__container");
+
+sliderContainers.forEach((sliderContainer) => {
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  sliderContainer.addEventListener("mousedown", (e) => {
+    isDown = true;
+    sliderContainer.classList.add("active");
+    startX = e.pageX - sliderContainer.offsetLeft;
+    scrollLeft = sliderContainer.scrollLeft;
+  });
+
+  document.addEventListener("mouseleave", () => {
+    isDown = false;
+    sliderContainer.classList.remove("active");
+  });
+
+  document.addEventListener("mouseup", () => {
+    isDown = false;
+    sliderContainer.classList.remove("active");
+  });
+
+  document.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - sliderContainer.offsetLeft;
+    const walk = (x - startX) * 5;
+    sliderContainer.scrollLeft = scrollLeft - walk;
+  });
+});
